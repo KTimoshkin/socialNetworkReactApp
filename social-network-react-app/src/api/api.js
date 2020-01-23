e4e217ea1776b5e90dc41d62117c25864de9e244
Константин Тимошкин <k.m.timoshkin@mail.ru>
2020-01-23 09:58:23 +0300

    Api doc

--- social-network-react-app/src/api/api.js
+++ social-network-react-app/src/api/api.js
@@ -7,6 +7,15 @@ const instance = axios.create({
 });
 
 export const friendsAPI = {
+
+    /**
+     * Получает список друзей на странице
+     *
+     * @param {number} currentPage - текущая страница
+     * @param {number} pageSize - размер страницы
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
     getFriends(currentPage, pageSize) {
         return(
             instance.get(`users?page=${currentPage}&count=${pageSize}`)
@@ -16,12 +25,28 @@ export const friendsAPI = {
         );
     },
 
+
+    /**
+     * Подписаться на пользователя
+     *
+     * @param {number} id - пользователь
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
     follow(id) {
         return(
             instance.post(`follow/${id}`, {})
         );
     },
 
+
+    /**
+     * Отписаться от пользователя
+     *
+     * @param {number} id - пользователь
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
     unfollow(id) {
         return(
             instance.delete(`follow/${id}`)
@@ -30,19 +55,41 @@ export const friendsAPI = {
 };
 
 export const authAPI = {
+
+    /**
+     * Аутентификация
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
     auth() {
         return(
             instance.get(`auth/me`)
         );
     },
 
+
+    /**
+     * Авторизация
+     *
+     * @param {string} email - Пользователь
+     * @param {string} password - Пароль
+     * @param {boolean} rememberme - Запомнить
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
     login(email, password, rememberme = false) {
         return(
             instance.post(`auth/login`, {email, password, rememberme})
         );
     },
 
-    logout(email, password, rememberme = false) {
+
+    /**
+     * Разлогин
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
+    logout() {
         return(
             instance.delete(`auth/login`)
         );
@@ -50,16 +97,42 @@ export const authAPI = {
 };
 
 export const profileAPI = {
+
+    /**
+     * Получить пользователя
+     *
+     * @param {number} userId - Пользователь
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
     getUser(userId) {
         return(
             instance.get(`profile/${userId}`)
         );
     },
+
+
+    /**
+     * Получить статус пользователя
+     *
+     * @param {number} userId - Пользователь
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
     getStatus(userId) {
         return(
             instance.get(`profile/status/${userId}`)
         );
     },
+
+
+    /**
+     * Обновить статус пользователя
+     *
+     * @param status - Новый статус
+     *
+     * @returns {Promise<AxiosResponse<T>>}
+     */
     updateStatus(status) {
         return(
             instance.put(`profile/status`, {status: status})
